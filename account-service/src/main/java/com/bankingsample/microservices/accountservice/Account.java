bb7547a757efc1cca487aedfb178f65fbfd557f9
Rishabh-Sharma1912 <rishab.sharma19@gmail.com>
2020-04-06 14:35:53 +0530

    Added additional fields to Account

--- account-service/src/main/java/com/bankingsample/microservices/accountservice/Account.java
+++ account-service/src/main/java/com/bankingsample/microservices/accountservice/Account.java
@@ -1,10 +1,12 @@
 package com.bankingsample.microservices.accountservice;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name="accounts")
@@ -28,18 +30,31 @@ public class Account {
 	@Column(name="current_balance")
 	private int currentBalance;
 	
+	@Column(name="currency_type")
+	private String currencyType;
+	
+	@Column(name="bsb")
+	private Integer bsb;
+	
+	@Column(name="creation_date")
+	@CreationTimestamp
+	private Date creationDate;
+	
 	public Account() {
 		
 	}
 
 	public Account(int accountId, @Size(min = 2) String accountName, @Size(min = 2) String accountType,
-			int availableBalance, int currentBalance) {
+			int availableBalance, int currentBalance, String currencyType, Integer bsb, Date creationDate) {
 		super();
 		this.accountId = accountId;
 		this.accountName = accountName;
 		this.accountType = accountType;
 		this.availableBalance = availableBalance;
 		this.currentBalance = currentBalance;
+		this.currencyType = currencyType;
+		this.bsb = bsb;
+		this.creationDate = creationDate;
 	}
 
 	public int getAccountId() {
@@ -81,4 +96,29 @@ public class Account {
 	public void setCurrentBalance(int currentBalance) {
 		this.currentBalance = currentBalance;
 	}
+
+	public String getCurrencyType() {
+		return currencyType;
+	}
+
+	public void setCurrencyType(String currencyType) {
+		this.currencyType = currencyType;
+	}
+
+	public Integer getBsb() {
+		return bsb;
+	}
+
+	public void setBsb(Integer bsb) {
+		this.bsb = bsb;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
 }
--- customer-service/src/main/java/com/bakingsample/micfroservices/customerservice/Account.java
+++ customer-service/src/main/java/com/bakingsample/micfroservices/customerservice/Account.java
@@ -1,45 +1,40 @@
 package com.bakingsample.micfroservices.customerservice;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
+import java.util.Date;
 
-@Entity
-@Table(name="accounts")
 public class Account {
 	
-	@Id
-	@Column(name="account_id")
 	private int accountId;
 	
-	@Size(min = 2)
-	@Column(name="account_name")
 	private String accountName;
 	
-	@Size(min = 2)
-	@Column(name="account_type")
 	private String accountType;
 	
-	@Column(name="available_balance")
 	private int availableBalance;
 	
-	@Column(name="current_balance")
 	private int currentBalance;
+	
+	private String currencyType;
+	
+	private Integer bsb;
+	
+	private Date creationDate;
 	
 	public Account() {
 		
 	}
 
-	public Account(int accountId, @Size(min = 2) String accountName, @Size(min = 2) String accountType,
-			int availableBalance, int currentBalance) {
+	public Account(int accountId, String accountName, String accountType, int availableBalance, int currentBalance,
+			String currencyType, Integer bsb, Date creationDate) {
 		super();
 		this.accountId = accountId;
 		this.accountName = accountName;
 		this.accountType = accountType;
 		this.availableBalance = availableBalance;
 		this.currentBalance = currentBalance;
+		this.currencyType = currencyType;
+		this.bsb = bsb;
+		this.creationDate = creationDate;
 	}
 
 	public int getAccountId() {
@@ -81,4 +76,29 @@ public class Account {
 	public void setCurrentBalance(int currentBalance) {
 		this.currentBalance = currentBalance;
 	}
+
+	public String getCurrencyType() {
+		return currencyType;
+	}
+
+	public void setCurrencyType(String currencyType) {
+		this.currencyType = currencyType;
+	}
+
+	public Integer getBsb() {
+		return bsb;
+	}
+
+	public void setBsb(Integer bsb) {
+		this.bsb = bsb;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
 }
